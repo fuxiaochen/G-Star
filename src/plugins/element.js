26039259c7aa6f4dc0c2b36cf7a19fb32798759b
fuxiaochen <1090879964@qq.com>
2021-03-01 15:31:31 +0800

    菜单

---
+++ src/plugins/element.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { Row, Col, Menu, MenuItem, MenuItemGroup, Submenu } from 'element-ui'
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Submenu)
