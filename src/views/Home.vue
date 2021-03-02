251a1d4e31aec8f701b60024998f2140c35f1ec9
fuxiaochen <1090879964@qq.com>
2021-03-02 18:18:39 +0800

    首页和内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,30 +52,44 @@
     <div class="details">
       <div class="top">
         <el-input
-          placeholder="请输入内容"
-          prefix-icon="el-icon-search"
-          v-model="input2"
+          placeholder="开发者|仓库名"
+          v-model="input1"
           clearable
-          style="width:35%"
-        ></el-input>
-        <el-input
-          placeholder="请输入内容"
-          prefix-icon="el-icon-search"
-          v-model="input2"
-          clearable
-          style="width:35%"
-        ></el-input>
+          style="width:30%; margin-left:15px">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+        <div class="user">
+          <img src="../assets/img/头像.png" alt="头像" class="avatar">
+          <div class="name">name</div>
+          <div class="user-content">
+          <div class="c-item"><router-link :to="'/system/baseInfo'"><i class="el-icon-tickets"></i> GitHUb</router-link></div>
+          <div class="c-item"><router-link :to="'/system/logMnt'"><i class="el-icon-reading"></i> 关注</router-link></div>
+          <div class="c-item"><router-link :to="'/system'"><i class="el-icon-setting"></i> 设置</router-link></div>
+          <div class="logout" @click="logoutEvent()"><i class="el-icon-switch-button"></i> 退出</div>
+        </div>
+        </div>
       </div>
       <div class="card">
-        <div class="left"></div>
+        <div class="left">
+          <div v-for ="i in 10"  class="box-card" :key="i">
+            <div class="title">{{'标题内容 ' + i }}</div>
+            <span class="text">{{'内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容 ' + i }}</span>
+            <div class="bottom">
+              <div class="left-star">121</div>
+              <div class="left-use">32</div>
+            </div>
+          </div>
+        </div>
         <div class="right">
-          <el-card class="box-card">
-            <div slot="header" class="clearfix">
+          <div class="right-content">
+            <div class="header">
               <span>卡片名称</span>
               <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
             </div>
-            <div v-for="o in 4" :key="o" class="text item">{{'列表内容' + o }}</div>
-          </el-card>
+            <div class="body">
+              <div v-for ="i in 50" :key="i">卡片名称</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -85,6 +99,12 @@
 <script>
 export default {
   name: 'Home',
+  data () {
+    return {
+      input1: '',
+      input2: ''
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -112,21 +132,143 @@ export default {
     background-color: #ffffff;
     .top {
       width: 100%;
-      height: 5%;
+      height: 6%;
       display: flex;
       justify-content: space-between;
-      align-items:center
+      align-items:center;
+      .user {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        .avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          padding: 0 10px;
+        }
+        .name {
+          cursor: pointer;
+          padding-right: 30px;
+          font-size: 22px;
+          color: #0d7cfc;
+        }
+        &:hover {
+          .user-content {
+            display: block;
+          }
+        }
+        .user-content {
+          display: none;
+          position: absolute;
+          right: 20px;
+          top: 56px;
+          background-color: #ffffff;
+          border: 1px solid #ffffff;
+          border-radius: 4px;
+          box-sizing: border-box;
+          min-width: 120px;
+          box-shadow: 0px 0px 8px 0px #5f86d4;
+          div{
+            padding: 10px 0 0;
+            cursor: pointer;
+            text-align: center;
+            font-size: 15px;
+            &:hover{
+              color: #07bed6;
+            }
+          }
+          a{
+            color: #46515a;
+            text-decoration: none;
+            padding: 10px 0;
+            &:hover{
+              color: #07bed6;
+            }
+          }
+          .c-item{
+            padding: 10px 0 0;
+          }
+          .logout{
+            padding-bottom: 10px;
+          }
+        }
+      }
     }
     .card {
-      display: flex;
+      width: 100%;
+      height: 94%;
+      display: flex; 
       justify-content: space-between;
       .left {
         width: 20%;
-        height: 95%;
+        height: 99%;
+        text-align: left;
+        overflow: auto;
+        .box-card {
+          border: 1px solid #EBEEF5;
+          padding: 10px;
+          margin: 0 5px;
+        }
+        .title {                             
+          color: #72c2f7;
+          font-size: 18px;
+          padding-bottom: 10px;
+          text-align: left;
+        }
+        .bottom{
+          display: flex;
+          padding-top: 10px;
+          justify-content: space-between;
+          align-items: center;
+          .left-star{
+            color: #7effd8;
+          }
+        }
+      }
+      .left::-webkit-scrollbar{//滚动条整体样式
+        width: 10px;
+        height: 10px;
+      }
+      .left::-webkit-scrollbar-thumb{//滚动条小方块样式
+        border-radius: 10px;
+        box-shadow: inset 0 0 5px rgba(255, 250, 250, 0.2);
+        background: #B5BECB;
+      }
+      .left::-webkit-scrollbar-track{//滚动条滚动轴样式
+        border-radius: 10px;
+        box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+        background: #ededed00;
       }
       .right {
         width: 80%;
-        height: 95%;
+        height: 100%;
+        padding: 0 0.5%;
+        .right-content {
+          height: 98%;
+          width: 100%;
+          border: 1px solid #EBEEF5;
+          border-radius: 4px;
+          box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+          .header {
+            height: 2%;
+            border-bottom: 1px solid #EBEEF5;
+            padding: 20px;
+          }
+          .body {
+            height: 89%;
+            padding: 10px;
+            overflow: auto;
+          }    
+          .body::-webkit-scrollbar {
+            display: none;
+          }
+        }
+        .right-card {
+          height:100%;
+          overflow:auto;
+        }
       }
     }
   }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="login">
-    <el-button class="btn" @click="authorization">登录</el-button>
+    <div class="sim-button button5" @click="authorization">
+      <span>授权</span>
+    </div>
   </div>
 </template>
 
@@ -8,10 +10,10 @@
 export default {
   name: 'Login',
   methods: {
-    authorization () {
+    authorization() {
       this.$router.push('/home')
-    }
-  }
+    },
+  },
 }
 </script>
 <style lang="scss" scoped>
@@ -19,20 +21,126 @@ export default {
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content:center;
+  justify-content: center;
   align-items: center;
   background-image: url(../assets/img/bg.jpg);
   background-repeat: no-repeat;
   background-size: 100% 100%;
-  .btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 150px;
+  .sim-button {
+    line-height: 50px;
     height: 50px;
-    font-size: 26px;
-    color: #ffffff;
-    background-color: rgba(194, 190, 190, 0.4);
+    text-align: center;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 125px;
+    width: 15%;
+    cursor: pointer;
+  }
+  @-webkit-keyframes jello {
+  from, 11.1%, to {
+    -webkit-transform: none;
+    transform: none;
+  }
+
+  22.2% {
+    -webkit-transform: skewX(-12.5deg) skewY(-12.5deg);
+    transform: skewX(-12.5deg) skewY(-12.5deg);
+  }
+
+  33.3% {
+    -webkit-transform: skewX(6.25deg) skewY(6.25deg);
+    transform: skewX(6.25deg) skewY(6.25deg);
+  }
+
+  44.4% {
+    -webkit-transform: skewX(-3.125deg) skewY(-3.125deg);
+    transform: skewX(-3.125deg) skewY(-3.125deg);
+  }
+
+  55.5% {
+    -webkit-transform: skewX(1.5625deg) skewY(1.5625deg);
+    transform: skewX(1.5625deg) skewY(1.5625deg);
+  }
+
+  66.6% {
+    -webkit-transform: skewX(-0.78125deg) skewY(-0.78125deg);
+    transform: skewX(-0.78125deg) skewY(-0.78125deg);
+  }
+
+  77.7% {
+    -webkit-transform: skewX(0.390625deg) skewY(0.390625deg);
+    transform: skewX(0.390625deg) skewY(0.390625deg);
+  }
+
+  88.8% {
+    -webkit-transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+    transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+  }
+}
+
+@keyframes jello {
+  from, 11.1%, to {
+    -webkit-transform: none;
+    transform: none;
+  }
+
+  22.2% {
+    -webkit-transform: skewX(-12.5deg) skewY(-12.5deg);
+    transform: skewX(-12.5deg) skewY(-12.5deg);
+  }
+
+  33.3% {
+    -webkit-transform: skewX(6.25deg) skewY(6.25deg);
+    transform: skewX(6.25deg) skewY(6.25deg);
+  }
+
+  44.4% {
+    -webkit-transform: skewX(-3.125deg) skewY(-3.125deg);
+    transform: skewX(-3.125deg) skewY(-3.125deg);
+  }
+
+  55.5% {
+    -webkit-transform: skewX(1.5625deg) skewY(1.5625deg);
+    transform: skewX(1.5625deg) skewY(1.5625deg);
+  }
+
+  66.6% {
+    -webkit-transform: skewX(-0.78125deg) skewY(-0.78125deg);
+    transform: skewX(-0.78125deg) skewY(-0.78125deg);
+  }
+
+  77.7% {
+    -webkit-transform: skewX(0.390625deg) skewY(0.390625deg);
+    transform: skewX(0.390625deg) skewY(0.390625deg);
+  }
+
+  88.8% {
+    -webkit-transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+    transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+  }
+}
+  .button5 {
+    color: rgba(255, 255, 255, 1);
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+    position: relative;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+  }
+  .button5 a {
+    color: rgba(51, 51, 51, 1);
+    text-decoration: none;
+    display: block;
+  }
+  .button5:hover {
+    -webkit-animation-name: jello;
+    animation-name: jello;
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    background-color: rgba(255, 255, 255, 0.2);
   }
 }
 </style>
